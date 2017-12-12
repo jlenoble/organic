43d3828b2877daa89ed042d60d56cbcb6f4a8d24
Jason Lenoble <jason.lenoble@gmail.com>
2017-12-12 16:28:56 +0100

    Removed newer/gulp plugins
    Rely on GulpTask@0.4 for that.
    
    Moreover inlined all exports.

--- gulp/helpers/pipes.js
+++ gulp/helpers/pipes.js
@@ -1,34 +1,16 @@
 import PolyPipe from 'polypipe';
 import gulp from 'gulp';
 import babel from 'gulp-babel';
-import newer from 'gulp-newer';
-import debug from 'gulp-debug';
 import antlr4 from 'gulp-antlr4';
-import antlr4Newer from 'gulp-antlr4-newer';
 
-import {buildDir, parserDir, listenerDir} from './dirs';
+import {parserDir, listenerDir} from './dirs';
 
 // Transpile pipeline
-const transpilePipe = new PolyPipe(
-  [newer, buildDir],
-  debug,
-  babel
-);
+export const transpilePipe = new PolyPipe(babel);
 
 // Make parser pipeline
-const makeParserPipe = new PolyPipe(
-  [antlr4Newer, parserDir],
-  debug,
-  [antlr4, parserDir]
-);
+export const makeParserPipe = new PolyPipe([antlr4, parserDir]);
 
 // Translate pipeline factory
-const translatePipe = ({
-  grammar, listener, rule, translationDir}) => new PolyPipe(
-  [newer, translationDir],
-  debug,
-  [antlr4, {grammar, listener, rule, parserDir, listenerDir}]
-);
-
-// Export all
-export {transpilePipe, makeParserPipe, translatePipe};
+export const translatePipe = ({grammar, listener, rule}) => new PolyPipe(
+  [antlr4, {grammar, listener, rule, parserDir, listenerDir}]);
