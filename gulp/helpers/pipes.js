0ebef2a449421b39a3283d40764387b48861a754
Jason Lenoble <jason.lenoble@gmail.com>
2017-12-14 01:03:26 +0100

    Externalized 'uncache' to own plugin

--- gulp/helpers/pipes.js
+++ gulp/helpers/pipes.js
@@ -2,37 +2,9 @@ import PolyPipe from 'polypipe';
 import gulp from 'gulp';
 import babel from 'gulp-babel';
 import antlr4 from 'gulp-antlr4';
+import uncache from 'gulp-require-uncache';
 
 import {parserDir, listenerDir} from './dirs';
-
-import {PluginError} from 'gulp-util';
-import through from 'through2';
-import path from 'path';
-
-const PLUGIN_NAME = 'gulp-require-uncache';
-
-function uncache () {
-  return require && require.cache ? through.obj(function (
-    file, encoding, done) {
-    if (file.isNull()) {
-      return done(null, file);
-    }
-
-    if (file.isStream()) {
-      this.emit( // eslint-disable-line no-invalid-this
-        'error', new PluginError(PLUGIN_NAME, 'Streams are not supported'));
-      return done();
-    }
-
-    if (file.isBuffer()) {
-      if (require.cache[file.path]) {
-        delete require.cache[file.path];
-      }
-
-      return done(null, file);
-    }
-  }) : gutil.noop();
-}
 
 // Transpile pipeline
 export const transpilePipe = new PolyPipe(babel);
