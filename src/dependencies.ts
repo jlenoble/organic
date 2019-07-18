20df00218a320aed8ddc7f86f80d48aa2a348488
Jason Lenoble <jason.lenoble@gmail.com>
2019-07-18 20:04:23 +0200

    Can now resolve globs outside cwd
    also can return found inconsistencies

--- src/dependencies.ts
+++ src/dependencies.ts
@@ -1,4 +1,4 @@
-import { resolveGlob } from "polypath";
+import { resolveGlob, rebaseGlob } from "polypath";
 import precinct from "precinct";
 import path from "path";
 
@@ -8,12 +8,21 @@ interface Deps {
 
 export default class Dependencies {
   public readonly ready: Promise<boolean>;
+
   protected _fromConfig: Deps;
   protected _fromFiles: Set<string>;
+
+  protected _localDeps: Set<string>;
+  protected _resolvedFiles: Set<string>;
 
   public constructor(glob: string | string[], deps: Deps) {
     this._fromConfig = { ...deps };
     this._fromFiles = new Set();
+    this._localDeps = new Set(
+      Object.keys(deps).filter((key): boolean => /^\.\.?\//.test(deps[key]))
+    );
+    this._resolvedFiles = new Set();
+
     this.ready = this._addGlob(glob).then((): true => true, (): false => false);
   }
 
@@ -37,6 +46,12 @@ export default class Dependencies {
     const files = await resolveGlob(glob);
 
     for (const file of files) {
+      if (this._resolvedFiles.has(file)) {
+        continue;
+      }
+
+      this._resolvedFiles.add(file);
+
       const deps = precinct.paperwork(file, {
         includeCore: false
       });
@@ -54,6 +69,20 @@ export default class Dependencies {
 
     return consistent;
   }
+
+  public async getInconsistencies(): Promise<string[]> {
+    await this.ready;
+
+    const deps: string[] = [];
+
+    for (const dep of this._fromFiles) {
+      if (this._fromConfig[dep] === undefined) {
+        deps.push(dep);
+      }
+    }
+
+    return deps;
+  }
 }
 
 export class ProdDependencies extends Dependencies {
@@ -61,7 +90,7 @@ export class ProdDependencies extends Dependencies {
     // eslint-disable-next-line @typescript-eslint/no-var-requires
     const pckg = require(path.join(packageDir, "package.json"));
 
-    super(glob, pckg.dependencies);
+    super(rebaseGlob(glob, packageDir), pckg.dependencies);
   }
 }
 
@@ -70,6 +99,9 @@ export class DevDependencies extends Dependencies {
     // eslint-disable-next-line @typescript-eslint/no-var-requires
     const pckg = require(path.join(packageDir, "package.json"));
 
-    super(glob, { ...pckg.dependencies, ...pckg.devDependencies });
+    super(rebaseGlob(glob, packageDir), {
+      ...pckg.dependencies,
+      ...pckg.devDependencies
+    });
   }
 }
--- test/dependencies.test.ts
+++ test/dependencies.test.ts
@@ -1,4 +1,6 @@
 import { expect } from "chai";
+import fse from "fs-extra";
+import path from "path";
 import { ProdDependencies, DevDependencies } from "../src/dependencies";
 
 describe("Testing Dependencies classes", (): void => {
@@ -48,5 +50,99 @@ describe("Testing Dependencies classes", (): void => {
     const deps = new DevDependencies("gulpfile.babel.js", process.cwd());
 
     expect(await deps.isEventuallyConsistent()).to.be.true;
+  });
+
+  it("All packages are consistent with regard to prod deps", async function(): Promise<
+    void
+  > {
+    this.timeout(20000); // eslint-disable-line no-invalid-this
+
+    const packages: string[] = await fse.readdir(
+      path.join(process.cwd(), "packages")
+    );
+
+    const allDeps = packages
+      .map((pckg): string => path.join(process.cwd(), "packages", pckg))
+      .map(
+        (pckg): ProdDependencies => new ProdDependencies("src/**/*.ts", pckg)
+      );
+
+    const consistencies = await Promise.all(
+      allDeps.map((dep): Promise<boolean> => dep.isEventuallyConsistent())
+    );
+
+    try {
+      expect(consistencies.every((consistency): boolean => consistency)).to.be
+        .true;
+    } catch (e) {
+      const indices: number[] = [];
+      const badPackages = allDeps.filter((dep, i): boolean => {
+        if (!consistencies[i]) {
+          indices.push(i);
+          return true;
+        }
+        return false;
+      });
+
+      const msg: string = (await Promise.all(
+        badPackages.map(
+          async (dep, i): Promise<string> => {
+            return `${JSON.stringify(
+              packages[indices[i]]
+            )} has inconsistent prod deps: ${JSON.stringify(
+              await dep.getInconsistencies()
+            )}`;
+          }
+        )
+      )).join("\n");
+
+      throw new Error(msg);
+    }
+  });
+
+  it("All packages are consistent with regard to dev deps", async function(): Promise<
+    void
+  > {
+    this.timeout(20000); // eslint-disable-line no-invalid-this
+
+    const packages: string[] = await fse.readdir(
+      path.join(process.cwd(), "packages")
+    );
+
+    const allDeps = packages
+      .map((pckg): string => path.join(process.cwd(), "packages", pckg))
+      .map((pckg): DevDependencies => new DevDependencies("src/**/*.ts", pckg));
+
+    const consistencies = await Promise.all(
+      allDeps.map((dep): Promise<boolean> => dep.isEventuallyConsistent())
+    );
+
+    try {
+      expect(consistencies.every((consistency): boolean => consistency)).to.be
+        .true;
+    } catch (e) {
+      const indices: number[] = [];
+      const badPackages = allDeps.filter((dep, i): boolean => {
+        if (!consistencies[i]) {
+          indices.push(i);
+          return true;
+        }
+        return false;
+      });
+
+      const msg: string = (await Promise.all(
+        badPackages.map(
+          async (dep, i): Promise<string> => {
+            return `${JSON.stringify(
+              packages[indices[i]]
+            )} has inconsistent prod deps: ${JSON.stringify(
+              await dep.getInconsistencies()
+            )}`;
+          }
+        )
+      )).join("\n");
+
+      throw new Error(msg);
+    }
   });
 });
