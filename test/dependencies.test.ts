f44f4675f36456fc1c837fa1bcfd6b94dc2c6ec2
Jason Lenoble <jason.lenoble@gmail.com>
2019-07-19 10:39:12 +0200

    Placed organon under self
    Removed 6 first tests, now redundant with last 2

--- test/dependencies.test.ts
+++ test/dependencies.test.ts
@@ -4,54 +4,6 @@ import path from "path";
 import { ProdDependencies, DevDependencies } from "../src/dependencies";
 
 describe("Testing Dependencies classes", (): void => {
-  it("ProdDependencies applied to src files is consistent", async (): Promise<
-    void
-  > => {
-    const deps = new ProdDependencies("src/**/*.ts", process.cwd());
-
-    expect(await deps.isEventuallyConsistent()).to.be.true;
-  });
-
-  it("ProdDependencies applied to test files is inconsistent", async (): Promise<
-    void
-  > => {
-    const deps = new ProdDependencies("test/**/*.ts", process.cwd());
-
-    expect(await deps.isEventuallyConsistent()).to.be.false;
-  });
-
-  it("ProdDependencies applied to gulp files is inconsistent", async (): Promise<
-    void
-  > => {
-    const deps = new ProdDependencies("gulp/**/*.js", process.cwd());
-
-    expect(await deps.isEventuallyConsistent()).to.be.false;
-  });
-
-  it("DevDependencies applied to src files is consistent", async (): Promise<
-    void
-  > => {
-    const deps = new DevDependencies("src/**/*.ts", process.cwd());
-
-    expect(await deps.isEventuallyConsistent()).to.be.true;
-  });
-
-  it("DevDependencies applied to test files is consistent", async (): Promise<
-    void
-  > => {
-    const deps = new DevDependencies("test/**/*.ts", process.cwd());
-
-    expect(await deps.isEventuallyConsistent()).to.be.true;
-  });
-
-  it("DevDependencies applied to gulp files is consistent", async (): Promise<
-    void
-  > => {
-    const deps = new DevDependencies("gulpfile.babel.js", process.cwd());
-
-    expect(await deps.isEventuallyConsistent()).to.be.true;
-  });
-
   it("All packages are consistent with regard to prod deps", async function(): Promise<
     void
   > {
