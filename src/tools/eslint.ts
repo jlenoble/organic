1299b42022aa5f311666e91c3ae45d3dc6ded7fd
Jason Lenoble <jason.lenoble@gmail.com>
2019-07-20 12:07:00 +0200

    Basic deps for eslint

--- src/tools/eslint.ts
+++ src/tools/eslint.ts
@@ -1,7 +1,27 @@
 import Deps, { Options } from "./common";
 
 export default class EslintConfig extends Deps {
-  public constructor(options: Options = {}) {
+  public constructor({ eslint, prettier, typescript }: Options = {}) {
     super();
+
+    if (eslint) {
+      this._addDeps(["eslint", "eslint-config-google"]);
+
+      if (prettier) {
+        this._addDeps([
+          "prettier",
+          "eslint-config-prettier",
+          "eslint-plugin-prettier"
+        ]);
+      }
+
+      if (typescript) {
+        this._addDeps([
+          "@typescript-eslint/eslint-plugin",
+          "@typescript-eslint/parser",
+          "@typescript-eslint/typescript-estree"
+        ]);
+      }
+    }
   }
 }
