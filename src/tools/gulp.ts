96c31ebff0a9ed055ef210b00322f2ab291e104e
Jason Lenoble <jason.lenoble@gmail.com>
2019-07-21 17:30:09 +0200

    Added more default deps (from gen wup 0.6.0)

--- src/tools/gulp.ts
+++ src/tools/gulp.ts
@@ -4,17 +4,32 @@ export default class GulpConfig extends Deps {
   public constructor(options: Options = {}) {
     super();
 
-    const { babel, eslint, gulp, mocha }: Options = options;
+    const { babel, eslint, gulp, mocha, typescript }: Options = options;
 
     if (gulp) {
-      this._addDeps(["gulp"]);
+      this._addDeps([
+        "gulp",
+        "plumb-gulp",
+        "autoreload-gulp",
+        "gulp-sourcemaps",
+        "gulp-cached",
+        "gulp-newer",
+        "del",
+        "gulp-rename",
+        "gulp-replace",
+        "gulp-wrap",
+        "ejs", // required by consolidate used by gulp-wrap
+        "markdown-include",
+        "polypath",
+        "stream-to-promise"
+      ]);
 
       if (babel) {
-        this._addDeps(["gulp-babel"]);
+        this._addDeps("gulp-babel");
       }
 
       if (eslint) {
-        this._addDeps(["gulp-eslint"]);
+        this._addDeps("gulp-eslint");
       }
 
       if (mocha) {
@@ -26,6 +41,10 @@ export default class GulpConfig extends Deps {
         ]);
 
         this._addDeps({ mocha: "<6" });
+      }
+
+      if (typescript) {
+        this._addDeps("gulp-typescript");
       }
     }
   }
