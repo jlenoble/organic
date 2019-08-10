a47aa3e865e7afde253c67328f408ce31f528bc7
Jason Lenoble <jason.lenoble@gmail.com>
2019-08-10 19:49:12 +0200

    Helper class EasyJson

---
+++ src/easy-json.ts
@@ -0,0 +1,211 @@
+interface GenericMap<T> {
+  [key: string]: T;
+}
+
+type GenericArray<T> = T[];
+type Primitive = string | number | boolean;
+type JsonValue = Primitive | JsonArray | JsonMap;
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+interface JsonArray extends GenericArray<JsonValue> {}
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+interface JsonMap extends GenericMap<JsonValue> {}
+
+type EasyType<T> = T extends Primitive
+  ? EasyPrimitive
+  : T extends JsonArray
+  ? EasyArray
+  : T extends JsonMap
+  ? EasyMap
+  : never;
+
+type EasyValue = EasyPrimitive | EasyArray | EasyMap;
+
+interface Easy {
+  value: JsonValue;
+  isAssignable(json: JsonValue): boolean;
+  deepAssign(json: JsonValue): this;
+}
+
+function easyFactory(json: JsonValue): EasyType<JsonValue> {
+  if (Array.isArray(json)) {
+    // eslint-disable-next-line @typescript-eslint/no-use-before-define
+    return new EasyArray(json);
+  }
+
+  switch (typeof json) {
+    case "object":
+      // eslint-disable-next-line @typescript-eslint/no-use-before-define
+      return new EasyMap(json);
+
+    case "string":
+    case "number":
+    case "boolean":
+      // eslint-disable-next-line @typescript-eslint/no-use-before-define
+      return new EasyPrimitive(json);
+  }
+}
+
+export class EasyPrimitive implements Easy {
+  protected _value: Primitive;
+
+  public get value(): Primitive {
+    return this._value;
+  }
+
+  public constructor(json: Primitive) {
+    this._value = json;
+  }
+
+  public isAssignable(json: JsonValue): boolean {
+    switch (typeof json) {
+      case "string":
+      case "number":
+      case "boolean":
+        return true;
+    }
+
+    return false;
+  }
+
+  public deepAssign(json: Primitive): this {
+    this._value = json;
+    return this;
+  }
+}
+
+export class EasyArray implements Easy {
+  protected _value: EasyValue[];
+
+  public get value(): JsonArray {
+    return this._value.map((easy): JsonValue => easy.value);
+  }
+
+  public constructor(json: JsonArray) {
+    this._value = new Array(json.length);
+
+    for (const [i, v] of json.entries()) {
+      this._value[i] = easyFactory(v);
+    }
+  }
+
+  public isAssignable(json: JsonValue): boolean {
+    return Array.isArray(json);
+  }
+
+  public deepAssign(json: JsonArray): this {
+    const len = this._value.length;
+
+    json.forEach((value, i): void => {
+      if (i < len && this._value[i].isAssignable(value)) {
+        // @ts-ignore
+        this._value[i].deepAssign(value);
+      } else {
+        this._value[i] = easyFactory(value);
+      }
+    });
+
+    return this;
+  }
+}
+
+export class EasyMap implements Easy {
+  protected _value: { [key: string]: EasyValue };
+
+  public get value(): JsonMap {
+    return Object.keys(this._value).reduce((json: JsonMap, key): JsonMap => {
+      json[key] = this._value[key].value;
+      return json;
+    }, {});
+  }
+
+  public constructor(json: JsonMap) {
+    this._value = {};
+
+    for (const key of Object.keys(json)) {
+      this._value[key] = easyFactory(json[key]);
+    }
+  }
+
+  public isAssignable(json: JsonValue): boolean {
+    return !Array.isArray(json) && typeof json === "object";
+  }
+
+  public deepAssign(json: JsonMap): this {
+    Object.keys(json).forEach((key): void => {
+      if (
+        this._value[key] !== undefined &&
+        this._value[key].isAssignable(json[key])
+      ) {
+        // @ts-ignore
+        this._value[key].deepAssign(json[key]);
+      } else {
+        this._value[key] = easyFactory(json[key]);
+      }
+    });
+
+    return this;
+  }
+}
+
+export default class EasyJson implements Easy {
+  protected _value: EasyType<JsonValue>;
+
+  public get value(): JsonValue {
+    return this._value.value;
+  }
+
+  public constructor(json: JsonValue) {
+    this._value = easyFactory(json);
+  }
+
+  public isAssignable(json: JsonValue): boolean {
+    switch (typeof json) {
+      case "object":
+      case "string":
+      case "number":
+      case "boolean":
+        return true;
+    }
+
+    return false;
+  }
+
+  public deepAssign(json: JsonValue): this {
+    if (Array.isArray(json)) {
+      return this._deepAssignArray(json);
+    }
+
+    switch (typeof json) {
+      case "object":
+        return this._deepAssignMap(json);
+
+      case "string":
+      case "number":
+      case "boolean":
+        this._value = new EasyPrimitive(json);
+    }
+
+    return this;
+  }
+
+  protected _deepAssignArray(json: JsonArray): this {
+    if (this._value instanceof EasyArray) {
+      this._value.deepAssign(json);
+    } else {
+      this._value = new EasyArray(json);
+    }
+
+    return this;
+  }
+
+  protected _deepAssignMap(json: JsonMap): this {
+    if (this._value instanceof EasyMap) {
+      this._value.deepAssign(json);
+    } else {
+      this._value = new EasyMap(json);
+    }
+
+    return this;
+  }
+}
---
+++ test/easy-json.test.ts
@@ -0,0 +1,47 @@
+import EasyJson from "../src/easy-json";
+import { expect } from "chai";
+
+describe("Testint EasyJson", (): void => {
+  it("Creating an EasyJson object", (): void => {
+    const json = { a: 1, b: true, c: [2, 3, 4], d: { e: ["hello"] } };
+    const easy = new EasyJson(json);
+
+    expect(easy.value).to.eql(json);
+  });
+
+  it("Deeply assigning an EasyJson object", (): void => {
+    const json1 = {
+      a: 1,
+      b: true,
+      c: [2, 3, 4],
+      d: { e: ["hello", "bye"] },
+      f: { a: { b: { c: { d: [{ e: [{ f: 4, u: 2 }, 14] }, 15] } } }, z: [5] }
+    };
+
+    const json2 = {
+      b: { e: 7, f: ["foo"] },
+      c: [3, 4],
+      d: { e: ["bye"], f: 21 },
+      g: 42,
+      f: { a: { b: { c: { d: [{ e: [{ f: "changed" }] }] } } }, x: true }
+    };
+
+    const json3 = {
+      a: 1,
+      b: { e: 7, f: ["foo"] },
+      c: [3, 4, 4],
+      d: { e: ["bye", "bye"], f: 21 },
+      g: 42,
+      f: {
+        a: { b: { c: { d: [{ e: [{ f: "changed", u: 2 }, 14] }, 15] } } },
+        z: [5],
+        x: true
+      }
+    };
+
+    const easy = new EasyJson(json1);
+    easy.deepAssign(json2);
+
+    expect(easy.value).to.eql(json3);
+  });
+});
