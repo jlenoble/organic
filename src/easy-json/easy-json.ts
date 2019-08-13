dba4f986d78ac8835d66b5cdfad9f4dfa6cb7498
Jason Lenoble <jason.lenoble@gmail.com>
2019-08-13 19:02:16 +0200

    Moved easyMap to corresponding class file

--- src/easy-json/easy-json.ts
+++ src/easy-json/easy-json.ts
@@ -1,55 +1,7 @@
-import { JsonValue, JsonMap } from "./json";
-import { EasyObjectProxy, EasyMapProxy } from "./easy";
+import { JsonValue } from "./json";
+import { EasyObjectProxy } from "./easy";
 import { easyArray } from "./easy-array";
-import EasyMap from "./easy-map";
-
-export function easyMap(json: JsonMap): EasyMapProxy {
-  const easy = new EasyMap();
-
-  Object.keys(json).forEach((key): void => {
-    // eslint-disable-next-line @typescript-eslint/no-use-before-define
-    easy.$[key] = easyJson(json[key]);
-  });
-
-  // @ts-ignore We cannot add a index signature to EasyMap because we cannot
-  // add the construct [key in Exclude<string, keyof Easy>] to it: it is
-  // interpreted as a computed property name.
-  const proxy: EasyMapProxy = new Proxy(easy, {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    get: (obj, prop): any => {
-      if (typeof prop === "string" && prop in obj.$) {
-        return obj.$[prop];
-      }
-
-      // @ts-ignore No index signature for EasyMap, see above explanation
-      return obj[prop];
-    },
-
-    set: (obj, prop, value): boolean => {
-      if (typeof prop === "string") {
-        // eslint-disable-next-line @typescript-eslint/no-use-before-define
-        obj.$[prop] = easyJson(value);
-        return true;
-      }
-
-      return false;
-    },
-
-    has: (obj, prop): boolean => {
-      return obj.$.hasOwnProperty(prop);
-    },
-
-    ownKeys: (obj): string[] => {
-      return Object.getOwnPropertyNames(obj.$);
-    },
-
-    getOwnPropertyDescriptor: (obj, prop): PropertyDescriptor | undefined => {
-      return Object.getOwnPropertyDescriptor(obj.$, prop);
-    }
-  });
-
-  return proxy;
-}
+import { easyMap } from "./easy-map";
 
 export default function easyJson(json: JsonValue): EasyObjectProxy {
   if (Array.isArray(json)) {
--- src/easy-json/easy-map.ts
+++ src/easy-json/easy-map.ts
@@ -99,3 +99,51 @@ export default class EasyMap implements Easy {
     });
   }
 }
+
+export function easyMap(json: JsonMap): EasyMapProxy {
+  const easy = new EasyMap();
+
+  Object.keys(json).forEach((key): void => {
+    // eslint-disable-next-line @typescript-eslint/no-use-before-define
+    easy.$[key] = easyJson(json[key]);
+  });
+
+  // @ts-ignore We cannot add a index signature to EasyMap because we cannot
+  // add the construct [key in Exclude<string, keyof Easy>] to it: it is
+  // interpreted as a computed property name.
+  const proxy: EasyMapProxy = new Proxy(easy, {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    get: (obj, prop): any => {
+      if (typeof prop === "string" && prop in obj.$) {
+        return obj.$[prop];
+      }
+
+      // @ts-ignore No index signature for EasyMap, see above explanation
+      return obj[prop];
+    },
+
+    set: (obj, prop, value): boolean => {
+      if (typeof prop === "string") {
+        // eslint-disable-next-line @typescript-eslint/no-use-before-define
+        obj.$[prop] = easyJson(value);
+        return true;
+      }
+
+      return false;
+    },
+
+    has: (obj, prop): boolean => {
+      return obj.$.hasOwnProperty(prop);
+    },
+
+    ownKeys: (obj): string[] => {
+      return Object.getOwnPropertyNames(obj.$);
+    },
+
+    getOwnPropertyDescriptor: (obj, prop): PropertyDescriptor | undefined => {
+      return Object.getOwnPropertyDescriptor(obj.$, prop);
+    }
+  });
+
+  return proxy;
+}
